88b4d307eed47b168e5f97f299d98b26f747f87b
Nadas <nalghofaili@gmail.com>
2017-02-06 16:36:06 -0800

    Sample Recommendations Page Added

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
 <?php
-if($_POST['formSubmit'] == "Submit")
+if($_POST['formSubmit'] == "Find Study Partners")
 {
 	$errorMessage = "";
 	
@@ -11,19 +11,28 @@ if($_POST['formSubmit'] == "Submit")
 	$varLang = $_POST['language'];
 	$varClass = $_POST['class'];
 	$varNumPartn = $_POST['numPartner'];
-	$varLocation = $_POST['location'];
+	$varLocationOn = $_POST['location1'];
+	$varLocationOff = $_POST['location2'];
+	$varMonTime = $_POST['MonTime'];
+	$varTueTime = $_POST['TueTime'];
+	$varWedTime = $_POST['WedTime'];
+	$varThuTime = $_POST['ThuTime'];
+	$varFriTime = $_POST['FriTime'];
 
 	if(empty($errorMessage)) 
 	{
 		$fs = fopen("data/mydata.csv","a");
-		fwrite($fs, $varName . ", " . $varMajor . ", " . 
-					$varSchool . ", " . $varEmail . ", " . 
-					$varPhone . ", " . $varLang . ", " . 
-					$varClass . ", " . $varNumPartn . ", " . 
-					$varLocation . "\n");
+		fwrite($fs, $varName 		. ", " . $varMajor 			. ", " . 
+					$varSchool 		. ", " . $varEmail 			. ", " . 
+					$varPhone 		. ", " . $varLang 			. ", " . 
+					$varClass 		. ", " . $varNumPartn 		. ", " . 
+					$varLocationOn 	. ", " . $varLocationOff 	. ", " .
+					$varMonTime 	. ", " . $varTueTime 	. ", " .
+					$varWedTime 	. ", " . $varThuTime 	. ", " .
+					$varFriTime 	. "\n");
 		fclose($fs);
 		
-		//header("Location: thankyou.html");
+		header("Location: recommendations.html");
 		exit;
 	}
 }
@@ -42,29 +51,91 @@ if($_POST['formSubmit'] == "Submit")
 
 			<!-- Header -->
 				<div id="header">
-
 					<!-- Inner -->
-						<div class="inner">
-							<form action="form.php" method="post" style="width: 100%">
+					<div class="inner">
+						<form action="form.php" method="post" style="width:inherit">
 								
-								<div class="innerleft">
+								<div class="col2">
 								    <input type="text" name="name" placeholder="Name" value="<?=$varName;?>"/>
 								    <input type="text" name="major" placeholder="Major" value="<?=$varMajor;?>"/>
 								    <input type="text" name="school" placeholder="School" value="<?=$varSchool;?>"/>
 								    <input type="text" name="email" placeholder="Email" value="<?=$varEmail;?>"/>
 								    <input type="text" name="phone" placeholder="Phone Number" value="<?=$varPhone;?>"/>
-								    <input type="text" name="language" placeholder="Language" value="<?=$varLang;?>"/>
+								    
 								</div>
 								
-								<div class="innerright">
+								<div class="col1">
+									<input type="text" name="language" placeholder="Language" value="<?=$varLang;?>"/>
 								    <input type="text" name="class" placeholder="Desired Class" value="<?=$varClass;?>"/>
 								    <input type="text" name="numPartner" placeholder="Max Partners" value="<?=$varNumPartn;?>"/>
-								    <input type="text" name="location" placeholder="On/Off Campus" value="<?=$varLocation;?>"/>
+								    Location Preference:<br>
+								    <input type="checkbox" name="location1" value="On"> On Campus<br>
+									<input type="checkbox" name="location2" value="Off"> Off Campus
 								</div>
-								<hr>
-							    <input type="submit" name="formSubmit" value="Submit" />
+								<div class="footer" style="background-color: rgba(255, 255, 255, 0.5);">
+									<table style="text-align: center;">
+										<tbody>
+										<tr>
+										<td>Monday</td>
+										<td>Tuesday</td>
+										<td>Wednesday</td>
+										<td>Thursday</td>
+										<td>Friday</td>
+										</tr>
+										<tr>
+										<td>
+											<select name="MonTime">
+											  <option value="morning">Morning</option>
+											  <option value="afternoon">Afternoon</option>
+											  <option value="evening">Evening</option>
+											  <option value="none">Can't This Day</option>
+											</select>
+										</td>
+										<td>
+											<select name="TueTime">
+											  <option value="morning">Morning</option>
+											  <option value="afternoon">Afternoon</option>
+											  <option value="evening">Evening</option>
+											  <option value="none">Can't This Day</option>
+											</select>
+										</td>
+										<td>
+											<select name="WedTime">
+											  <option value="morning">Morning</option>
+											  <option value="afternoon">Afternoon</option>
+											  <option value="evening">Evening</option>
+											  <option value="none">Can't This Day</option>
+											</select>
+										</td>
+										<td>
+											<select name="ThuTime">
+											  <option value="morning">Morning</option>
+											  <option value="afternoon">Afternoon</option>
+											  <option value="evening">Evening</option>
+											  <option value="none">Can't This Day</option>
+											</select>
+										</td>
+										<td>
+											<select name="FriTime">
+											  <option value="morning">Morning</option>
+											  <option value="afternoon">Afternoon</option>
+											  <option value="evening">Evening</option>
+											  <option value="none">Can't This Day</option>
+											</select>
+										</td>
+										</tr>
+										</tbody>
+										</table>
+								</div>
+								
+								<div class="footer">
+									<input type="submit" name="formSubmit" value="Find Study Partners" class="formbutton"/>
+								</div>
+
+
+							   
 							</form>
-						</div>
+					</div>
 
 					<!-- Nav -->
 						<nav id="nav">
